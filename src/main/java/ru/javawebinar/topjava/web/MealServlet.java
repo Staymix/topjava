475f29b3a4a42ecb1978cbb85241116a31f80965
Staymix <staszubov199530@gmail.com>
2023-08-06 17:46:43 +0600

    Add logging

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -32,40 +32,50 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.debug("redirect to users");
         String action = req.getParameter("action");
         if (action == null) {
+            log.info("Action = null");
             req.setAttribute("meals", MealsUtil.getWithExceeded((List<Meal>) repository.getMeals(), LIMIT_CALORIES));
             req.getRequestDispatcher("/meals.jsp").forward(req, resp);
             return;
         }
         Meal meal;
+        int id;
         switch (action) {
             case "delete":
-                repository.deleteMeal(Integer.valueOf(req.getParameter("id")));
+                id = Integer.parseInt(req.getParameter("id"));
+                log.info("Deleting meal with id: " + id);
+                repository.deleteMeal(id);
                 resp.sendRedirect("meals");
                 return;
             case "update":
-                meal = repository.getMeal(Integer.parseInt(req.getParameter("id")));
+                id = Integer.parseInt(req.getParameter("id"));
+                log.info("Updating meal with id: " + id);
+                meal = repository.getMeal(id);
                 break;
             case "create":
+                log.info("Creating new Meal");
                 meal = Meal.getUserMeal();
                 break;
             default:
+                log.info("Default, Unknown action");
                 req.setAttribute("meals", MealsUtil.getWithExceeded((List<Meal>) repository.getMeals(), LIMIT_CALORIES));
                 req.getRequestDispatcher("/meals.jsp").forward(req, resp);
                 return;
         }
         req.setAttribute("meals", meal);
+        log.info("Forwarding to editMeal.jsp");
         req.getRequestDispatcher("/editMeal.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("Processing a meal addition request...");
         req.setCharacterEncoding("UTF-8");
         Integer id = (req.getParameter("id").isEmpty() ? null : Integer.parseInt(req.getParameter("id")));
         repository.addMeal(new Meal(LocalDateTime.parse(req.getParameter("date"), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")),
                 req.getParameter("description"), Integer.parseInt(req.getParameter("calories")), id));
         resp.sendRedirect("meals");
+        log.info("Meal added successfully.");
     }
 }
