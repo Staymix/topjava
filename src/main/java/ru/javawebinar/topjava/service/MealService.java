7e2d69ad5ad3bd0a1a1f586b20da4c3c6c58acbd
Staymix <staszubov199530@gmail.com>
2023-08-10 19:24:05 +0600

    Implement application layers for 'food' functionality

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,40 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.ValidationUtil;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal save(Meal meal) {
+        return repository.save(authUserId(), meal);
+    }
+
+    public Meal update(Meal meal) {
+        return ValidationUtil.checkNotFound(repository.save(authUserId(), meal), String.valueOf(authUserId()));
+    }
+
+    public void delete(int id) {
+        ValidationUtil.checkNotFoundWithId(repository.delete(authUserId(), id), id);
+    }
+
+    public Meal get(int id) {
+        return ValidationUtil.checkNotFound(repository.get(authUserId(), id), String.valueOf(id));
+    }
+
+    public List<Meal> getAll() {
+        return (List<Meal>) repository.getAll(authUserId());
+    }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,49 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-public class MealRestController {
-    private MealService service;
+import java.util.List;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
+public class MealRestController {
+
+    private final MealService service;
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal save(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.save(meal);
+    }
+
+    public Meal update(Meal meal) {
+        log.info("update {}", meal);
+        return service.update(meal);
+    }
+
+    public List<Meal> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public Meal get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
 }
