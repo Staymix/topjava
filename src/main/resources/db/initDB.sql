df9b3599ac0fdb4c7ae8956e30f58d0b1aae0fc8
Staymix <staszubov199530@gmail.com>
2023-08-25 20:25:26 +0600

    Add meals table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -6,20 +7,31 @@ CREATE SEQUENCE global_seq START WITH 100000;
 
 CREATE TABLE users
 (
-    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    name             VARCHAR                           NOT NULL,
-    email            VARCHAR                           NOT NULL,
-    password         VARCHAR                           NOT NULL,
-    registered       TIMESTAMP           DEFAULT now() NOT NULL,
-    enabled          BOOL                DEFAULT TRUE  NOT NULL,
-    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
+    id               integer PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_name        varchar                           NOT NULL,
+    email            varchar                           NOT NULL,
+    password         varchar                           NOT NULL,
+    registered       timestamp           DEFAULT now() NOT NULL,
+    enabled          bool                DEFAULT TRUE  NOT NULL,
+    calories_per_day integer             DEFAULT 2000  NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_role
 (
-    user_id INTEGER NOT NULL,
-    role    VARCHAR NOT NULL,
+    user_id integer NOT NULL,
+    role    varchar NOT NULL,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          serial     PRIMARY KEY,
+    user_id     integer     REFERENCES users(id) ON DELETE CASCADE,
+    date_time   timestamp   NOT NULL,
+    description varchar(32) NOT NULL,
+    calories    integer     NOT NULL,
+    UNIQUE (user_id, date_time)
+);
+CREATE UNIQUE INDEX user_meals_date_time_idx ON meals (date_time);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,7 +2,7 @@ DELETE FROM user_role;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password)
+INSERT INTO users (user_name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
@@ -10,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES  (100000, '2023-01-01 10:23:00', 'завтрак', '500'),
+        (100000, '2023-01-01 13:23:00', 'обед', '800'),
+        (100000, '2023-01-01 17:23:00', 'ужин', '1000'),
+        (100001, '2023-01-01 09:23:00', 'завтрак', '700'),
+        (100001, '2023-01-01 12:23:00', 'обед', '900'),
+        (100001, '2023-01-01 16:23:00', 'ужин', '800');
+
