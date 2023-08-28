99210a2f5932459b81807016cb2842c85d18aece
Staymix <staszubov199530@gmail.com>
2023-08-28 13:37:45 +0600

    Refactor various sections of code

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -11,33 +11,39 @@ import java.util.List;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
-    public static final int MEAL_ID = 100003;
-    public static final int USER_ID = START_SEQ;
-    public static final int ADMIN_ID = START_SEQ + 1;
+    public static final int USER_MEAL_ID = START_SEQ + 3;
     public static final int MEAL_NOT_FOUND = 999999999;
-    public static final Meal MEAL = new Meal(MEAL_ID, LocalDateTime.of(2023, Month.JANUARY, 1, 6, 23, 0), "завтрак", 859);
-    public static final Meal NEW_MEAL = new Meal(100021, LocalDateTime.of(2023, Month.JANUARY, 1, 10, 23, 0), "завтрак", 500);
+    public static final Meal userMeal = new Meal(USER_MEAL_ID, LocalDateTime.of(2023, Month.JANUARY, 1, 6, 23, 0), "завтрак", 859);
+    public static final Meal userMeal2 = new Meal(USER_MEAL_ID + 1, LocalDateTime.of(2023, Month.JANUARY, 1, 11, 23, 0), "обед", 458);
+    public static final Meal userMeal3 = new Meal(USER_MEAL_ID + 2, LocalDateTime.of(2023, Month.JANUARY, 1, 20, 23, 0), "ужин", 745);
+    public static final Meal userMeal4 = new Meal(USER_MEAL_ID + 3, LocalDateTime.of(2023, Month.JANUARY, 2, 8, 23, 0), "завтрак", 785);
+    public static final Meal userMeal5 = new Meal(USER_MEAL_ID + 4, LocalDateTime.of(2023, Month.JANUARY, 2, 14, 23, 0), "обед", 565);
+    public static final Meal userMeal6 = new Meal(USER_MEAL_ID + 5, LocalDateTime.of(2023, Month.JANUARY, 2, 21, 23, 0), "ужин", 985);
+    public static final Meal userMeal7 = new Meal(USER_MEAL_ID + 6, LocalDateTime.of(2023, Month.JANUARY, 3, 8, 23, 0), "завтрак", 653);
+    public static final Meal userMeal8 = new Meal(USER_MEAL_ID + 7, LocalDateTime.of(2023, Month.JANUARY, 3, 12, 23, 0), "обед", 845);
+    public static final Meal userMeal9 = new Meal(USER_MEAL_ID + 8, LocalDateTime.of(2023, Month.JANUARY, 3, 23, 23, 0), "ужин", 456);
+    public static final Meal createMeal = new Meal(USER_MEAL_ID + 18, LocalDateTime.of(2020, Month.JANUARY, 10, 11, 10, 10), "description", 999);
     public static final LocalDate START_DATE = LocalDate.of(2023, Month.JANUARY, 2);
     public static final LocalDate END_DATE = LocalDate.of(2023, Month.JANUARY, 3);
-    public static final List<Meal> MEALS_USER = Arrays.asList(
-            new Meal(100011, LocalDateTime.of(2023, Month.JANUARY, 3, 23, 23, 0), "ужин", 456),
-            new Meal(100010, LocalDateTime.of(2023, Month.JANUARY, 3, 12, 23, 0), "обед", 845),
-            new Meal(100009, LocalDateTime.of(2023, Month.JANUARY, 3, 8, 23, 0), "завтрак", 653),
-            new Meal(100008, LocalDateTime.of(2023, Month.JANUARY, 2, 21, 23, 0), "ужин", 985),
-            new Meal(100007, LocalDateTime.of(2023, Month.JANUARY, 2, 14, 23, 0), "обед", 565),
-            new Meal(100006, LocalDateTime.of(2023, Month.JANUARY, 2, 8, 23, 0), "завтрак", 785),
-            new Meal(100005, LocalDateTime.of(2023, Month.JANUARY, 1, 20, 23, 0), "ужин", 745),
-            new Meal(100004, LocalDateTime.of(2023, Month.JANUARY, 1, 11, 23, 0), "обед", 458),
-            new Meal(100003, LocalDateTime.of(2023, Month.JANUARY, 1, 6, 23, 0), "завтрак", 859)
+    public static final List<Meal> userMeals = Arrays.asList(
+            new Meal(userMeal9),
+            new Meal(userMeal8),
+            new Meal(userMeal7),
+            new Meal(userMeal6),
+            new Meal(userMeal5),
+            new Meal(userMeal4),
+            new Meal(userMeal3),
+            new Meal(userMeal2),
+            new Meal(userMeal)
     );
 
-    public static final List<Meal> MEALS_USER_BETWEEN_INCLUSIVE = Arrays.asList(
-            new Meal(100011, LocalDateTime.of(2023, Month.JANUARY, 3, 23, 23, 0), "ужин", 456),
-            new Meal(100010, LocalDateTime.of(2023, Month.JANUARY, 3, 12, 23, 0), "обед", 845),
-            new Meal(100009, LocalDateTime.of(2023, Month.JANUARY, 3, 8, 23, 0), "завтрак", 653),
-            new Meal(100008, LocalDateTime.of(2023, Month.JANUARY, 2, 21, 23, 0), "ужин", 985),
-            new Meal(100007, LocalDateTime.of(2023, Month.JANUARY, 2, 14, 23, 0), "обед", 565),
-            new Meal(100006, LocalDateTime.of(2023, Month.JANUARY, 2, 8, 23, 0), "завтрак", 785)
+    public static final List<Meal> userMealsBetweenInclusive = Arrays.asList(
+            new Meal(userMeal9),
+            new Meal(userMeal8),
+            new Meal(userMeal7),
+            new Meal(userMeal6),
+            new Meal(userMeal5),
+            new Meal(userMeal4)
     );
 
     public static Meal getNew() {
@@ -45,10 +51,10 @@ public class MealTestData {
     }
 
     public static Meal getUpdated() {
-        return new Meal(MEAL_ID, LocalDateTime.of(1999, Month.JANUARY, 10, 10, 10, 10, 10), "update", 1);
+        return new Meal(USER_MEAL_ID, LocalDateTime.of(1999, Month.JANUARY, 10, 10, 10, 10), "update", 1);
     }
 
     public static Meal getDuplicateDateTime() {
-        return new Meal(LocalDateTime.of(2023, Month.JANUARY, 1, 20, 23, 0), "duplicate", 1000);
+        return new Meal(userMeal.getDateTime(), "duplicateByDateTime", 1000);
     }
 }
